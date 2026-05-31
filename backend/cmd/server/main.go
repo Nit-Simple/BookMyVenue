52ba547100784246d5590cdc5b7f753baea0c640
nithul <nithulshamej2@gmail.com>
2026-05-31 21:07:58 +0530

    fixed git ignore issue

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Nit-Simple/BookMyVenue/internal/config"
+	"github.com/Nit-Simple/BookMyVenue/internal/handler"
+	"github.com/Nit-Simple/BookMyVenue/internal/repository"
+	"github.com/Nit-Simple/BookMyVenue/pkg/logger"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load config %v\n", err)
+		os.Exit(1)
+	}
+	logger := logger.New(cfg.Environment)
+	db, err := repository.Connect(context.Background(), cfg)
+	if err != nil {
+		logger.Error("failed to connect to database", "err", err)
+		os.Exit(1)
+	}
+	cache, err := repository.NewRedisConnection(ctx, cfg)
+	if err != nil {
+		logger.Error("failed to connect to redis", "err", err)
+		os.Exit(1)
+	}
+
+	server := handler.NewServer(cfg, db, logger, cache)
+	if err := server.Start(); err != nil {
+		logger.Error("unable to start the server", "err", err)
+		os.Exit(1)
+	}
+}
